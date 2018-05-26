af6c2ea1878c2c6831b88e08de39fcaa407e7614
wahyu sumartha <wahyusumartha@gmail.com>
2018-05-26 13:18:03 +0200

    Initial configuration

---
+++ dist/handler.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const hello = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Go Serverless v1.0! Your function executed successfully!",
+            input: event,
+        })
+    };
+    callback(undefined, response);
+};
+exports.hello = hello;
+//# sourceMappingURL=handler.js.map
