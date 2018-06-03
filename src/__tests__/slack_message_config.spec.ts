9395112ee8e2210c9987128a32e6b128cdc92565
Gigih Iski Prasetyawan <gigihiskiprasetyawan@gmail.com>
2018-06-04 04:17:42 +0700

    +Add Unit Test for Configuration Loader

---
+++ src/__tests__/slack_message_config.spec.ts
@@ -0,0 +1,47 @@
+import { Configurator } from "../config/slack_message_config"
+
+describe("Slack Message Configuration", () => {
+	let configurator: Configurator = undefined
+
+	describe("when there is file configuration", () => {
+		beforeEach(() => {
+			configurator = new Configurator("env.example.yml")
+		})
+
+		it("should return correct bot token", () => {
+			expect(configurator.getSlackConfig().botToken).toEqual(
+				"<YOUR_BOT_ACCESS_TOKEN>"
+			)
+		})
+
+		it("should return correct auth token", () => {
+			expect(configurator.getSlackConfig().authToken).toEqual(
+				"<YOUR_OAUTH_ACCESS_TOKEN>"
+			)
+		})
+
+		it("should return correct channel name", () => {
+			expect(configurator.getSlackConfig().channelName).toEqual(
+				"<YOUR_CHANNEL_NAME>"
+			)
+		})
+
+		it("should return correct channel destination", () => {
+			expect(configurator.getSlackConfig().channel).toEqual(
+				"<YOUR_CHANNEL_DESTINATION_ID>"
+			)
+		})
+	})
+
+	describe("when there is no file configuration", () => {
+		beforeEach(() => {
+			configurator = new Configurator("env.whatever.yml")
+		})
+
+		it("should throw an exception", () => {
+			expect(() => configurator.getSlackConfig()).toThrowError(
+				"Error: ENOENT: no such file or directory, open 'env.whatever.yml'"
+			)
+		})
+	})
+})
