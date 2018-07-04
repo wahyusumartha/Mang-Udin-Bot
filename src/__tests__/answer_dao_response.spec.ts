6068e65748204c29ff9d86d948bbe5fe3099b8a4
Gigih Iski Prasetyawan <gigihiskiprasetyawan@gmail.com>
2018-07-05 00:17:31 +0700

    +Answer DAO Unit Test

---
+++ src/__tests__/answer_dao_response.spec.ts
@@ -0,0 +1,92 @@
+import { AnswerDAO } from "../database/dao/answer_dao"
+import { DatabaseConfigurator } from "../database/database_configurator"
+import { Answer } from "../database/models/Answer"
+import { QuestionDAO } from "../database/dao/question_dao"
+import {
+	QuestionPersistentModel,
+	AnswerPersistentModel
+} from "../model/persistent/persistent_type"
+
+describe("Answer DAO", () => {
+	const databaseConfigurator: DatabaseConfigurator = new DatabaseConfigurator(
+		"127.0.0.1",
+		"mangudin-db",
+		"mangudin",
+		"mamangudin",
+		"postgres"
+	)
+
+	let savedAnswer: Answer
+
+	beforeEach(async () => {
+		savedAnswer = await prepareData()
+	})
+
+	afterEach(async () => {
+		const sequelize = databaseConfigurator.getSequelize()
+		await sequelize.truncate()
+		sequelize.close()
+	})
+
+	test("Get Answer By ID", async () => {
+		const answerDAO = new AnswerDAO(databaseConfigurator)
+		const answer = await answerDAO.getAnswerById(savedAnswer.id)
+		expect(answer).not.toBeNull()
+		expect(answer.slackId).toEqual("1")
+		expect(answer.slackMessageId).toEqual("1")
+		expect(answer.answerText).toEqual("Answer Text")
+		expect(answer.questionId).toEqual(savedAnswer.questionId)
+		expect(answer.id).toEqual(savedAnswer.id)
+	})
+
+	test("Save and Retrieve Answer Data", async () => {
+		const answerDAO = new AnswerDAO(databaseConfigurator)
+		const allAnswerData = await answerDAO.getAnswers()
+		expect(allAnswerData.length).toEqual(1)
+	})
+
+	test("Update Answer Data", async () => {
+		const answerDAO = new AnswerDAO(databaseConfigurator)
+		const newAnswerData: AnswerPersistentModel = {
+			slackId: "2",
+			slackMessageId: "2",
+			answerText: "New Answer Text",
+			questionId: savedAnswer.questionId
+		}
+		const isUpdated = await answerDAO.updateAnswer(
+			savedAnswer.id,
+			newAnswerData
+		)
+		expect(isUpdated[0]).toEqual(1)
+	})
+
+	test("Delete Answer Data", async () => {
+		const answerDAO = new AnswerDAO(databaseConfigurator)
+		const isDeleted = await answerDAO.deleteAnswer(savedAnswer.id)
+		expect(isDeleted).toEqual(1)
+	})
+
+	// Helper Method
+	const prepareData = async (): Promise<Answer> => {
+		const questionDAO = new QuestionDAO(databaseConfigurator)
+		const questionPersistentModel: QuestionPersistentModel = {
+			questionText: "Question Text",
+			order: 1
+		}
+		const savedQuestion = await questionDAO.saveQuestion(
+			questionPersistentModel
+		)
+
+		// save answer based on question
+		const answerDAO = new AnswerDAO(databaseConfigurator)
+		const answerPersistentModel: AnswerPersistentModel = {
+			slackId: "1",
+			slackMessageId: "1",
+			answerText: "Answer Text",
+			questionId: savedQuestion.id
+		}
+		const savedAnswer = await answerDAO.saveAnswer(answerPersistentModel)
+
+		return savedAnswer
+	}
+})
