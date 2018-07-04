660c1fa1ba8b56bbd77813c9a37e8f9307a9a055
Gigih Iski Prasetyawan <gigihiskiprasetyawan@gmail.com>
2018-07-05 00:18:01 +0700

    +Question and Answer Migrations File

---
+++ src/database/migrations/20180630212238-create-answer.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Answers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      slackId: {
+        type: Sequelize.STRING
+      },
+      slackMessageId: {
+        type: Sequelize.STRING
+      },
+      answerText: {
+        type: Sequelize.STRING
+      },
+      questionId: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Answers');
+  }
+};
---
+++ src/database/migrations/20180630212617-create-question.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      questionText: {
+        type: Sequelize.STRING
+      },
+      order: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Questions');
+  }
+};
