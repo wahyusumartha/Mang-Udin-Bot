71ab7fa0bd0dc2bc063694ee1320482bc208fa0c
wahyu sumartha <wahyusumartha@gmail.com>
2018-07-08 21:22:09 +0200

    Dedicated file to sync database

---
+++ src/database/sync.ts
@@ -0,0 +1,39 @@
+import { DatabaseConfigurator } from "./database_configurator"
+import { JSONReader, Environment } from "../helper/test/file_manager"
+
+const env = process.argv[2]
+
+let environment: Environment
+
+switch (env) {
+	case "test": {
+		environment = Environment.Test
+		break
+	}
+	case "development": {
+		environment = Environment.Development
+		break
+	}
+	case "production": {
+		environment = Environment.Production
+		break
+	}
+}
+
+const dbJSON = new JSONReader(environment).read("config.json")
+const dbConfigurator = new DatabaseConfigurator(
+	dbJSON.host,
+	dbJSON.database,
+	dbJSON.username,
+	dbJSON.password,
+	dbJSON.dialect
+)
+const sequelize = dbConfigurator.getSequelize()
+sequelize
+	.sync()
+	.then(() => {
+		process.exit(1)
+	})
+	.catch(() => {
+		process.exit(1)
+	})
