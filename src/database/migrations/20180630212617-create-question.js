3f588f5a8fafa6dbb08ff4856e57444320366a02
Gigih Iski Prasetyawan <gigihiskiprasetyawan@gmail.com>
2018-07-20 02:51:19 +0700

    +Change Table Name Questions to Question

--- src/database/migrations/20180630212617-create-question.js
+++ src/database/migrations/20180630212617-create-question.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Questions', {
+    return queryInterface.createTable('Question', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Questions');
+    return queryInterface.dropTable('Question');
   }
 };
