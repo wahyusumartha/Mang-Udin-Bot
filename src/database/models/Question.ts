01d09b27c4d1e764ca60908ffab8cede431921c3
Gigih Iski Prasetyawan <gigihiskiprasetyawan@gmail.com>
2018-07-20 02:55:43 +0700

    +Change Question[] to Question on Answer Model

--- src/database/models/Question.ts
+++ src/database/models/Question.ts
@@ -1,11 +1,11 @@
 import { Table, Column, Model, HasMany } from "sequelize-typescript"
 import { Answer } from "./Answer"
-
 @Table({
 	timestamps: true
 })
 export class Question extends Model<Question> {
 	@Column questionText: string
+
 	@Column order: number
 
 	@HasMany(() => Answer)
